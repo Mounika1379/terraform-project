0a3cca3b50f61d124f83b5279f3c4033cae8b3ab
Mounika1379 <123626361+Mounika1379@users.noreply.github.com>
2023-03-22 18:15:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIA3OWTCUQABEO6WZWU"
-secret_key = "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"
+access_key = "AKIA3OWTCUQACKQBTXYZ"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
